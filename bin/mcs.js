1b3a761060ae4692200e2deaf199c5d9951ce439
Bartek Nowotarski <bartek@nowotarski.info>
2015-03-22 17:30:40 +0100

    CLI fix

--- bin/mcs.js
+++ bin/mcs.js
@@ -1,7 +1,7 @@
+#!/usr/bin/env node
 var cwd = process.cwd();
 var spawn = require('child_process').spawn;
 var child = spawn(cwd+'/node_modules/mcs-core/node_modules/gulp/bin/gulp.js', [
   '--cwd', '.',
-  '--gulpfile', 'node_modules/mcs-core/gulpfile.js',
-  'build'
-], {stdio: 'inherit'});
+  '--gulpfile', 'node_modules/mcs-core/gulpfile.js'
+].concat(process.argv.slice(2)), {stdio: 'inherit'});
